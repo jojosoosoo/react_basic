e6bf89962d73909524a68b5c0d07191bd341c974
jojosoosoo <hyeoneegirl@naver.com>
2024-04-16 10:35:11 +0900

    docs: 컴포넌트

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,16 @@
-import React, { Component } from "react";
-import "./react.css";
+import React from "react";
 
-const Main = props => {
+const Main = ({ title, children }) => {
   return (
     <div>
-      <h1>안녕하세요, 나는 {props.title}입니다.</h1>
+      <h1>안녕하세요, 나는 {title}입니다.</h1>
+      <h2>childern 값은 {children}입니다.</h2>
     </div>
   );
+};
+
+Main.defaultProps = {
+  title: "기본 이름",
 };
 
 export default Main;
